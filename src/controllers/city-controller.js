08c02b30526bd97932d13f93aad9d900150805fe
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-02 15:06:34 +0530

    2nd June 2024

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -2,7 +2,6 @@ const { CityService } = require('../services/index');
 
 const cityService = new CityService();
 
-// POST. -> /city
 const create = async (req, res) => {
     try {
         const city = await cityService.createCity(req.body);
@@ -64,7 +63,7 @@ const get = async (req, res) => {
     }
 }
 
-// Patch -> /city/:id -> req.body {parameters}
+// Patch -> /city/:id -> req.body
 const update = async (req, res) => {
     try {
         const response = await cityService.updateCity(req.params.id, req.body);
@@ -85,30 +84,30 @@ const update = async (req, res) => {
     }
 }
 
-// const getAll = async (req, res) => {
-//     try {
-//         const cities = await cityService.getAllCities(req.query);
-//         return res.status(200).json({
-//             data: cities,
-//             success: true,
-//             message: 'Successfully fetched all cities',
-//             err: {}
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             data: {},
-//             success: false,
-//             message: 'Not able to fetch the cities',
-//             err: error
-//         });
-//     }
-// }
+const getAll = async (req, res) => {
+    try {
+        const cities = await cityService.getAllCities(req.query);
+        return res.status(200).json({
+            data: cities,
+            success: true,
+            message: 'Successfully fetched all cities',
+            err: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the cities',
+            err: error
+        });
+    }
+}
 
 module.exports = {
     create,
     destroy,
     get,
     update,
-    // getAll
+    getAll
 }
