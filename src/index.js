2c53c718bf594cc1a5a727a859b0828526175069
AKcode07 <ashwinkumavat07@gmail.com>
2024-06-03 16:49:59 +0530

    3rd June 2024

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
-// const db = require('./models/index');
+const db = require('./models/index');
 // const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
@@ -19,9 +19,9 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        // if(process.env.SYNC_DB) {
-        //     db.sequelize.sync({alter: true});
-        // }
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
     });
 }
 
